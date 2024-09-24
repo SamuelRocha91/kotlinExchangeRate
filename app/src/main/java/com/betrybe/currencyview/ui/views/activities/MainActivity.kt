9e78b4b8e1aad38465deb13da61fd3c2f8277405
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-23 21:54:30 -0300

    feat: refatora código tornando-o mais legível

--- app/src/main/java/com/betrybe/currencyview/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/currencyview/ui/views/activities/MainActivity.kt
@@ -3,10 +3,10 @@ package com.betrybe.currencyview.ui.views.activities
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -25,67 +25,101 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private val apiKey = BuildConfig.API_KEY;
-
-    val loadingCurrency: MaterialTextView by lazy { findViewById(R.id.load_currency_state) }
-    val selectOptions: AutoCompleteTextView by lazy { findViewById(R.id.currency_selection_input_layout) }
-    val selectCurrency: MaterialTextView by lazy { findViewById(R.id.select_currency_state) }
-    val recyclerView: RecyclerView by lazy { findViewById(R.id.currency_rates_state) }
     private val apiService = ApiLayer.instance;
+
+    private val loadingCurrency: MaterialTextView by lazy { findViewById(R.id.load_currency_state) }
+    private val selectOptions: AutoCompleteTextView by lazy { findViewById(R.id.currency_selection_input_layout) }
+    private val selectCurrency: MaterialTextView by lazy { findViewById(R.id.select_currency_state) }
+    private val recyclerView: RecyclerView by lazy { findViewById(R.id.currency_rates_state) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        searchCurrency()
+        fetchCurrencies()
     }
 
-    private fun searchCurrency() {
+    private fun fetchCurrencies() {
         loadingCurrency.visibility = View.VISIBLE;
-
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 ApiIdlingResource.increment()
-
-                val currencies = apiService.getSymbols(apiKey).body()?.symbols?.keys?.toList();
-
+                val currenciesSymbols =
+                    apiService.getSymbols(apiKey).body()?.symbols?.keys?.toList();
                 withContext(Dispatchers.Main) {
-                    currencies?.let {
-                        loadingCurrency.visibility = View.GONE;
-                        selectCurrency.visibility = View.VISIBLE;
-                        val adapter = ArrayAdapter(baseContext, android.R.layout.simple_dropdown_item_1line, currencies)
-                        selectOptions.setAdapter(adapter)
-                        selectOptions.setOnItemClickListener{ parent, view, position, id ->
-                            val selectedCurrency = parent.getItemAtPosition(position) as String
-                           CoroutineScope(Dispatchers.IO).launch {
-                               ApiIdlingResource.increment()
-                               val lates =  apiService.getRates(apiKey, selectedCurrency);
-                               Log.d("rate", "aqui")
-                               Log.d("rate", lates.toString())
-                               val rates = lates.body()
-                               withContext(Dispatchers.Main) {
-                                   rates?.let {
-                                       Log.d("rate", rates.toString())
-                                       selectCurrency.visibility = View.GONE;
-                                       recyclerView.layoutManager =
-                                           LinearLayoutManager(baseContext);
-                                       recyclerView.adapter = RateAdapter(rates)
-                                       recyclerView.visibility = View.VISIBLE;
-
-                                   }
-                               }
-                               ApiIdlingResource.decrement()
-
-                           }
-                        }
-                    } ?: kotlin.run {
-                        Toast.makeText(baseContext, "Deu ruim", Toast.LENGTH_LONG).show()
+                    if (currenciesSymbols != null) {
+                        renderCurrenciesOptions(currenciesSymbols)
+                    } else {
+                        Toast.makeText(
+                            baseContext,
+                            "Erro no carregamento das moedas. Favor, tente mais tarde.",
+                            Toast.LENGTH_LONG
+                        ).show()
                     }
                 }
                 ApiIdlingResource.decrement()
             } catch (e: HttpException) {
+                Log.e("ApiError", "Erro na resposta da API", e)
                 ApiIdlingResource.decrement()
-
+                Toast.makeText(
+                    baseContext,
+                    "Erro na comunicação com o servidor. Tente novamente.",
+                    Toast.LENGTH_LONG
+                ).show()
             } catch (e: IOException) {
-                // ADICIONAR ESSA LINHA
+                Log.e("NetworkError", "Erro de conectividade", e)
+                Toast.makeText(baseContext, "Erro de conectividade", Toast.LENGTH_LONG).show()
+                ApiIdlingResource.decrement()
+            }
+        }
+    }
+
+    private fun renderCurrenciesOptions(currencies: List<String>) {
+        loadingCurrency.visibility = View.GONE;
+        selectCurrency.visibility = View.VISIBLE;
+        val adapter = ArrayAdapter(
+            baseContext,
+            android.R.layout.simple_dropdown_item_1line,
+            currencies
+        )
+        selectOptions.setAdapter(adapter)
+        selectOptions.setOnItemClickListener { parent, _, position, _ ->
+            handleOptionClickAndRenderRates(parent,  position)
+        }
+    }
+
+    private fun handleOptionClickAndRenderRates(
+        parent: AdapterView<*>,
+        position: Int,
+    ) {
+        val selectedCurrency = parent.getItemAtPosition(position) as String
+        CoroutineScope(Dispatchers.IO).launch {
+            ApiIdlingResource.increment();
+            try {
+                val rates = apiService.getRates(apiKey, selectedCurrency).body();
+                withContext(Dispatchers.Main) {
+                    if (rates != null) {
+                        selectCurrency.visibility = View.GONE;
+                        recyclerView.layoutManager =
+                            LinearLayoutManager(baseContext);
+                        recyclerView.adapter = RateAdapter(rates)
+                        recyclerView.visibility = View.VISIBLE;
+                    } else {
+                        Toast.makeText(baseContext, "Por favor, tente novamente", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                    ApiIdlingResource.decrement()
+                }
+            } catch (e: HttpException) {
+                Log.e("ApiError", "Erro na resposta da API", e)
+                ApiIdlingResource.decrement()
+                Toast.makeText(
+                    baseContext,
+                    "Erro na comunicação com o servidor. Tente novamente.",
+                    Toast.LENGTH_LONG
+                ).show()
+            } catch (e: IOException) {
+                Log.e("NetworkError", "Erro de conectividade", e)
+                Toast.makeText(baseContext, "Erro de conectividade", Toast.LENGTH_LONG).show()
                 ApiIdlingResource.decrement()
             }
         }
